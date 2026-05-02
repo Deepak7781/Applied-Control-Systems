b76c5a45ac5aa9eb6b069534f07bad35c70723b7
Deepak7781 <deepak21082005@gmail.com>
2026-05-02 20:55:19 +0530

    update: waterTankControl.py

--- Water Tank/waterTankControl.py
+++ Water Tank/waterTankControl.py
@@ -2,3 +2,14 @@ import control as ctrl
 import numpy as np
 
 s = ctrl.tf('s')
+
+rho = 1000 # Density of water in kg/m^3
+
+G = 1/(rho*s) # Transfer function of the system (Tank)
+
+Kp = 0.5 # Proportional gain
+
+# Closed-loop transfer function with proportional control
+T = ctrl.feedback(Kp*G, 1)
+
+ctrl.step_response(T)
